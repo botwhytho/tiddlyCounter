f2d421d0112163acfe6bcc5cb0c50890bb101f5b
Alberto <botwhytho@gmail.com>
2017-02-22 22:16:31 -0500

    Start/Stop Logic and UI working! Added preliminary logic for rate but need to check the calculation because the rate increases when I click the stop button, which doesn't seem right.

--- newCounter.js
+++ newCounter.js
@@ -26,10 +26,12 @@ Run the macro
 */
 
 exports.run = function(counterList) {
-    var tmp = "";
-    counterList.split("|").forEach(function(element, index, array){
-		tmp = tmp + "<br><br><$button>" + element + " | {{" + element + "!!count}} <$action-setfield $tiddler='" + element + "' $field='count' $value=<<plusOne '" + element + "' 'count'>> /><$action-setfield $tiddler='" + element + "' text=<<plusOne '" + element + "' 'data'>> type='application/json' /></$button>";
+		var items = counterList.split("|");
+    var tmp = '<$importvariables filter="[[$:/plugins/botwhytho/tiddlyCounter]plugintiddlers[]]">';
+    items.forEach(function(element, index, array){
+		tmp = tmp + '<<tc_newItem "' + element + '">><br><br>';
     })
+		tmp = tmp + "</$importvariables>";
     return tmp;
 }
 
